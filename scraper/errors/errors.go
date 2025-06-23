3fbe200effc842f2799efd5f6650a862cf04b88d
Advik <advik.b@gmail.com>
2025-06-23 14:06:54 +0530

    Create errors.go

---
+++ scraper/errors/errors.go
@@ -0,0 +1,13 @@
+package errors
+
+import "errors"
+
+var (
+	ErrCloudflare         = errors.New("cloudflare error")
+	ErrChallenge          = errors.New("challenge error")
+	ErrUnknownChallenge   = errors.New("unknown cloudflare challenge")
+	ErrChallengeTimeout   = errors.New("challenge solving timeout")
+	ErrNoCaptchaSolver    = errors.New("captcha provider not configured")
+	ErrAllProxiesBanned   = errors.New("all proxies are currently banned")
+	ErrMaxRetriesExceeded = errors.New("failed after max retries")
+)
